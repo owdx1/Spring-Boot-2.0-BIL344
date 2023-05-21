662d3f8f6229ac2e3d8ad0c35646ea2a9de3a32c
Can Bozkurt <can.bozkurt.dhf@gmail.com>
2023-05-21 10:33:40 +0300

    Unused import statements are removed,  added .gitattributes file

--- demo/src/main/java/com/example/demo/model/UserService.java
+++ demo/src/main/java/com/example/demo/model/UserService.java
@@ -1,15 +1,9 @@
 package com.example.demo.model;
 
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.server.ResponseStatusException;
-
-import javax.sql.DataSource;
-import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
